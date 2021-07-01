cbfc7f2e2dd1f468c8e9aabd9645cbbb3b229746
NixonVuele <nixonvuele51@gmail.com>
2021-07-01 15:33:20 -0500

    version1

--- ProyectoTaller/src/paquete2/PasajeInterCantonal.java
+++ ProyectoTaller/src/paquete2/PasajeInterCantonal.java
@@ -11,14 +11,82 @@ import java.io.Serializable;
  *
  * @author reroes
  */
-public abstract class PasajeInterCantonal implements Serializable{
-    
-    
-    
+public abstract class PasajeInterCantonal implements Serializable {
+
+    protected String nombrePasajero;
+    protected String cedula;
+    protected String origen;
+    protected String destino;
+    protected int kmDistancia;
+    protected double tarifaBase;
+    protected double valorPasaje;
+
+    public PasajeInterCantonal(String nombrePasajero, String cedula,
+            String destino, int kmDistancia, double tarifaBase) {
+        this.nombrePasajero = nombrePasajero;
+        this.cedula = cedula;
+        this.destino = destino;
+        this.kmDistancia = kmDistancia;
+        this.tarifaBase = tarifaBase;
+    }
+
+    public void setNombrePasajero(String nombrePasajero) {
+        this.nombrePasajero = nombrePasajero;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public void setKmDistancia(int kmDistancia) {
+        this.kmDistancia = kmDistancia;
+    }
+
+    public void setTarifaBase(double tarifaBase) {
+        this.tarifaBase = tarifaBase;
+    }
+
+    public abstract void setValorPasaje();
+
+    public String getNombrePasajero() {
+        return nombrePasajero;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public int getKmDistancia() {
+        return kmDistancia;
+    }
+
+    public double getTarifaBase() {
+        return tarifaBase;
+    }
+
+    public double getValorPasaje() {
+        return valorPasaje;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return String.format("Pasaje.");
     }
-    
-    
+
 }
--- ProyectoTaller/src/paquete2/PasajeMenorEdad.java
+++ ProyectoTaller/src/paquete2/PasajeMenorEdad.java
@@ -11,9 +11,29 @@ import java.io.Serializable;
  *
  * @author reroes
  */
-public class PasajeMenorEdad extends PasajeInterCantonal 
-        implements Serializable{
-    
-    
-    
+public class PasajeMenorEdad extends PasajeInterCantonal
+        implements Serializable {
+
+    private int porcentajeDescuento;
+
+    public PasajeMenorEdad(int porcentajeDescuento, String nombrePasajero,
+            String cedula, String destino, int kmDistancia, double tarifaBase) {
+        super(nombrePasajero, cedula, destino, kmDistancia, tarifaBase);
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+
+    public void setPorcentajeDescuento(int porcentajeDescuento) {
+        this.porcentajeDescuento = porcentajeDescuento;
+    }
+
+    public int getPorcentajeDescuento() {
+        return porcentajeDescuento;
+    }
+
+    @Override
+    public void setValorPasaje() {
+        valorPasaje = (kmDistancia * 0.10)
+                + (tarifaBase - (tarifaBase * (porcentajeDescuento / 100)));
+    }
+
 }
